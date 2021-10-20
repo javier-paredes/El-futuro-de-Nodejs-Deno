3a54100723a8db00379a8bfb63807206b0bd4203
Javier Alejandro Paredes <javier.aleparedes@gmail.com>
2021-10-19 23:43:55 -0300

    Entrega deasfio clase 47

--- server.tsx
+++ server.tsx
@@ -1,6 +1,6 @@
-// @deno-types="https://deno.land/x/servest@v1.3.1/types/react/index.d.ts"
+// @deno-types="https://deno.land/x/servest@v1.3.4/types/react/index.d.ts"
 import React from "https://dev.jspm.io/react/index.js";
-// @deno-types="https://deno.land/x/servest@v1.3.1/types/react-dom/server/index.d.ts"
+// @deno-types="https://deno.land/x/servest@v1.3.4/types/react-dom/server/index.d.ts"
 import ReactDOMServer from "https://dev.jspm.io/react-dom/server.js";
 import { contentTypeFilter, createApp } from "https://deno.land/x/servest@v1.3.4/mod.ts";
 
